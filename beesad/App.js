15a45652fe219caea6105e7fa386735f1a8deac5
emulberg <emulberg@stud.ntnu.no>
2018-10-07 14:19:41 +0200

    issue #7 made a rudimentary todoApp

--- beesad/App.js
+++ beesad/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Todo from "./app/components/Todo";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+
+        <Todo> </Todo>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });

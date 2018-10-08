78c4258492a0d149b609910de3342f6eaeb08c5f
tordsta <tord.standnes@gmail.com>
2018-10-08 17:29:14 +0200

    #6 implementert bottom navigation bar uten styling

--- beesad/App.js
+++ beesad/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation';
+import { Icon } from 'react-native-elements';
 import Todo from "./Components/Todo";
-import NavTest from "./Components/NavTest";
-import {createStackNavigator} from 'react-navigation';
+import Routing from "./Components/Routing";
 
-const RootStack = createStackNavigator({
-    Home: {screen: NavTest},
-    Todo: {screen: Todo},
-},  {
-    initialRouteName: 'Todo',
-}
+
+const RootStack = createBottomTabNavigator({
+        Home: {screen: Routing},
+        Todo: {screen: Todo},
+    },
+    {
+        initialRouteName: 'Home',
+    },
+    /*this needs styling, the styling don't work*/
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'a') {
+                    iconName = <Icon name='rowing' />;
+                } else if (routeName === 'b') {
+                    iconName = <Icon name='rowing' />;
+                }
+
+                // You can return any component that you like here! We usually use an
+                // icon component from react-native-vector-icons
+                return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+        },
+    }
 );
 
 export default class App extends React.Component {
---
+++ beesad/Components/Routing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, Button, StyleSheet, View } from 'react-native';
+
+
+class Routing extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Home Screen, just make a new homescreen when time</Text>
+            </View>
+        );
+    }
+}
+
+export default Routing;

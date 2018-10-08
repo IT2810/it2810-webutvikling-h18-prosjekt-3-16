881fbca2450bdcbceb9686446c9144cd62b95968
tordsta <tord.standnes@gmail.com>
2018-10-08 14:58:25 +0200

    #6 installert og implementert funksjonel routing uten gui

--- beesad/App.js
+++ beesad/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Todo from "./Components/Todo";
+import NavTest from "./Components/NavTest";
+import {createStackNavigator} from 'react-navigation';
+
+const RootStack = createStackNavigator({
+    Home: {screen: NavTest},
+    Todo: {screen: Todo},
+},  {
+    initialRouteName: 'Todo',
+}
+);
 
 export default class App extends React.Component {
-  render() {
-    return (
-        <Todo> </Todo>
-    );
-  }
+    render() {
+        return <RootStack />;
+    }
 }
 
 const styles = StyleSheet.create({
---
+++ beesad/Components/NavTest.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+class NavTest extends React.Component {
+    render() {
+        return (
+            <Text>hello world, NavTest</Text>
+        );
+    }
+}
+
+export default NavTest;

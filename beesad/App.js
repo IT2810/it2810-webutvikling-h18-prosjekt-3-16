0605a2316ef51b12fd2b01056fc897e8924dc82e
auk <oyvind.auk@gmail.com>
2018-10-17 10:17:47 +0200

    adding pedometer module. not yet working

--- beesad/App.js
+++ beesad/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Todo from "./Components/Todo";
+import Pedometer from "./Components/Pedometer";
 
 export default class App extends React.Component {
   render() {
     return (
-        <Todo> </Todo>
+        <Todo />
     );
   }
 }

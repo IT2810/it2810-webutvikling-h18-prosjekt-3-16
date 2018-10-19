fb2bd3d6fd731afc5ef4a26d9b18461c315c04a7
tordsta <tord.standnes@gmail.com>
2018-10-19 20:42:09 +0200

    none

--- beesad/Components/Todo.test.js
+++ beesad/Components/Todo.test.js
@@ -6,3 +6,16 @@ test('renders correctly', () => {
     const tree = renderer.create(<Todo />).toJSON();
     expect(tree).toMatchSnapshot();
 });
+
+test('test if add note function is called', () => {
+
+    let array = [1,2,3,4,5]
+    const tree = renderer.create(<Todo/>).getInstance();
+    console.log(tree.noteArray);
+    tree.noteArray = array;
+    console.log(tree.noteArray);
+    tree.deleteNote(3);
+    expect(Object.is(tree.noteArray, [1,2,4,5]));
+
+});
+ex
